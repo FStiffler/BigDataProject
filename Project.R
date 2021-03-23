e4008bf22e7fdab59b9cad86f0bfc8160641e1ec
FStiffler <fstiffler@gmx.ch>
2021-03-23 17:01:05 +0100

    Load Data (not yet optimized) and define methods and variables for analysis

--- Project.R
+++ Project.R
@@ -1,5 +1,20 @@
 library(jsonlite)
 library(tidyverse)
 
-business<-stream_in(file("yelp_academic_dataset_business.json"))
-business <- fromJSON(sprintf("[%s]", paste(readLines("yelp_academic_dataset_business.json"), collapse=",")), simplifyVector=TRUE, simplifyDataFrame=TRUE, simplifyMatrix=TRUE , flatten=TRUE)
+business <- fromJSON(sprintf('[%s]', paste(readLines('yelp_academic_dataset_business.json'), collapse=',')), simplifyVector=TRUE, simplifyDataFrame=TRUE, simplifyMatrix=TRUE , flatten=TRUE)
+review<-fromJSON(sprintf('[%s]', paste(readLines('yelp_academic_dataset_review.json'), collapse=',')), simplifyVector=TRUE, simplifyDataFrame=TRUE, simplifyMatrix=TRUE , flatten=TRUE)
+user<-fromJSON(sprintf('[%s]', paste(readLines('yelp_academic_dataset_user.json'), collapse=',')), simplifyVector=TRUE, simplifyDataFrame=TRUE, simplifyMatrix=TRUE , flatten=TRUE)
+
+#Analyzes:
+#The effect of stars assigned on average in reviews on the probability of being a elite user (Uluru OLS)
+#-indepenent variable: average stars
+#-covariates: quality of reviews
+#-dependent variable: binary elite or nor
+
+#What are the most important factors for restaurants to have good reviews (forward selection)
+#-dependent: stars of restaurant
+
+
+
+
+
