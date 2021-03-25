c80f158e1aaf3340857dc5ef66fb9b4c3eee706b
FStiffler <fstiffler@gmx.ch>
2021-03-25 18:17:21 +0100

    File changes to set up next working stage

--- Project.R
+++ Project.R
@@ -3,6 +3,9 @@
 #-indepenent variable: average stars
 #-covariates: quality of reviews
 #-dependent variable: binary elite or nor
+#-assumptions: 
+##-User does not change rating behaviour over time
+##-We do not consider if a user is elite over multiple years
 
 #What are the most important factors for restaurants to have good reviews (forward selection)
 #-dependent: stars of restaurant
