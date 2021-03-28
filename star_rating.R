9682157fbb9d8183c1da2f632c417f6f0cfac030
DESKTOP-4BDDAHB\sthan <s.thanusan@hotmail.ch>
2021-03-28 15:17:07 +0200

    code added

--- star_rating.R
+++ star_rating.R
@@ -28,7 +28,78 @@ df<-data_prep2omit
 df[df==""]<-NA
 df<-df[complete.cases(df),]
 
+#transform data to numercs(dummy variable), TRUE= 1, FALSE=0
+df %<>% mutate_if(is.logical,as.numeric) 
+#repalcing char values with logical 
+df2<-df%>%select(1,2,4:25,27:33,36)
+
+#creating dummy variables df3
+df3<-df
+df3$attributes.WiFi[df3$attributes.WiFi== "no"]<- 0
+df3$attributes.WiFi[df3$attributes.WiFi== "paid"]<- 1
+df3$attributes.WiFi[df3$attributes.WiFi== "free"]<- 2
+
+df3$attributes.Alcohol[df3$attributes.Alcohol== "none"]<- 0
+df3$attributes.Alcohol[df3$attributes.Alcohol== "beer_and_wine"]<- 1
+df3$attributes.Alcohol[df3$attributes.Alcohol== "full_bar"]<- 2
+
+
+df3$attributes.NoiseLevel[df3$attributes.NoiseLevel== "very_loud"]<-0
+df3$attributes.NoiseLevel[df3$attributes.NoiseLevel== "loud"]<- 1
+df3$attributes.NoiseLevel[df3$attributes.NoiseLevel== "average"]<- 2
+df3$attributes.NoiseLevel[df3$attributes.NoiseLevel== "quiet"]<- 3
+
+df3$attributes.RestaurantsAttire[df3$attributes.RestaurantsAttire== "dressy"]<- 0
+df3$attributes.RestaurantsAttire[df3$attributes.RestaurantsAttire== "casual"]<- 1
+
+cor <- round(cor(df3[,c(2:36)]),2) 
+corrplot(cor)
+
+ols <- lm(stars ~ ., data = df3)
+summary(ols)
+
+#lasso 
+lasso <- glmnet(as.matrix(df3[,c(2:36)]), df3$stars, alpha = 1) # We save the model under the name "lasso"
+plot(lasso, xvar = "lambda", label = TRUE)
+
+#cross validation for finding the right lamda value
+lasso.cv <- cv.glmnet(as.matrix(df3[,c(2:36)]), df3$stars, type.measure = "mse", nfolds = 5, alpha = 1)
+
+plot(lasso.cv)
+
+print(paste0("Optimal lambda that minimizes cross-validated MSE: ", lasso.cv$lambda.min))
+print(paste0("Optimal lambda using one-standard-error-rule: ", lasso.cv$lambda.1se))
+
+# Print Lasso coefficients
+print(coef(lasso.cv, s = "lambda.min"))
 
 
 
+# alpha = 0 specifies a Ridge model
+
+# Estimate the Ridge
+ridge <- glmnet(as.matrix(df3[,c(2:36)]), df3$stars, alpha = 0)
+
+# Plot the path of the Ridge coefficients
+plot(ridge, xvar = "lambda", label = TRUE)
+
+
+# Cross-validate the Ridge model 
+ridge.cv <- cv.glmnet(as.matrix(df3[,c(2:36)]), df2$stars, type.measure = "mse", nfolds = 5, alpha = 0)
+
+# Plot the MSE in the cross-validation samples
+plot(ridge.cv)
+
+# Print the optimal lambda value
+print(paste0("Optimal lambda that minimizes cross-validated MSE: ", ridge.cv$lambda.min))
+print(paste0("Optimal lambda using one-standard-error-rule: ", ridge.cv$lambda.1se))
+
+
+####################  Ridge Coefficients  ########################
+
+# Print Ridge coefficients
+print(coef(ridge.cv, s = "lambda.min"))
+
+# Save for later comparison
+coef_ridge <- coef(ridge.cv, s = "lambda.min") 
 
