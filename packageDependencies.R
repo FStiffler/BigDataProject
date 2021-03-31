0548f457d3cb447ab4b21d3a7c4c101aae5e527e
FStiffler <fstiffler@gmx.ch>
2021-03-31 12:05:54 +0200

    Deleted files

--- packageDependencies.R
+++
@@ -1,7 +0,0 @@
-##### Set dependencies
-dependecies <- c("jsonlite","tidyverse","data.table", "glment")
-
-##### Install and load packages
-missing <- dependecies[!(dependecies %in% installed.packages()[,"Package"])]
-if(length(missing)) install.packages(missing)
-lapply(dependecies, library, character.only = T)
